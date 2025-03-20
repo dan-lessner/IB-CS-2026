13d4deea0a73b4e6d061c815cae6dbf1ed9d957e
Karafakeu <karafak7@gmail.com>
2025-03-20 20:32:40 +0100

    Add __name__ condition to LL

--- LinkedList.py
+++ LinkedList.py
@@ -67,13 +67,15 @@ class LinkedList:
             current = current.next
         print("None")
 
-# Example usage:
-ll = LinkedList()
-ll.append(1)
-ll.append(2)
-ll.append(3)
-ll.print_list()  # Output: 1 -> 2 -> 3 -> None
+# Here so that when imported it doesnt run random bs
+if __name__ == "__main__":
+    # Example usage:
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.print_list()  # Output: 1 -> 2 -> 3 -> None
 
-node_to_remove = ll.head.next  # Node with value 2
-ll.remove_node(node_to_remove)
-ll.print_list()  # Output: 1 -> 3 -> None
+    node_to_remove = ll.head.next  # Node with value 2
+    ll.remove_node(node_to_remove)
+    ll.print_list()  # Output: 1 -> 3 -> None
