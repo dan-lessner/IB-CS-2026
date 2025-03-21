a6a9867ecfca5d72c0dd33c1950c9df0aee40271
Oldík <ozahr2016@gmail.com>
2025-03-21 11:44:46 +0100

    quicksort

--- Quicksort lepší.py
+++ Quicksort lepší.py
@@ -1,7 +1,7 @@
 import random
 array = []
 values_number = int(input("Number of values in the list "))
-list_range = int(input("How many digits you want to create? "))
+list_range = int(input("How many digits you want to create? (The number you put in will act as an exponential to number 10) "))
 
 for i in range(0, values_number):
     n = random.randint(1, 10**list_range)
